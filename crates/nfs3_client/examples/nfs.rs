a407e6fa4749485e29723359366535a1da8a768a
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-03-02 09:49:25 +0100

    unpack result

--- crates/nfs3_client/examples/nfs.rs
+++ crates/nfs3_client/examples/nfs.rs
@@ -54,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let access = connection.access(nfs3::ACCESS3args {
         object: root.clone(),
         access: 0,
-    }).await;
+    }).await?;
     println!("access: {access:?}");
 
     println!("Calling readdir");
