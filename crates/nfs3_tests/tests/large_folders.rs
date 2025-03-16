7f130b971f891e566771f46a7f87294666bcb576
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-03-16 15:01:41 +0100

    large dir tests

--- crates/nfs3_tests/tests/large_folders.rs
+++ crates/nfs3_tests/tests/large_folders.rs
@@ -1,6 +1,37 @@
+use nfs3_client::io::{AsyncRead, AsyncWrite};
 use nfs3_tests::{FsConfig, TestContext};
-use nfs3_types::nfs3::{cookieverf3, dirlist3, diropargs3, filename3, nfs_fh3, LOOKUP3args, READDIR3args};
+use nfs3_types::nfs3::{
+    cookieverf3, dirlist3, dirlistplus3, diropargs3, filename3, nfs_fh3, LOOKUP3args, READDIR3args, READDIRPLUS3args
+};
 use nfs3_types::xdr_codec::{Opaque, PackedSize};
+use tracing::info;
+
+
+#[ignore = "wip"]
+#[tokio::test]
+async fn test_10() {
+    test_dir(10, "dir_10").await.unwrap();
+}
+
+#[ignore = "wip"]
+#[tokio::test]
+async fn test_100() {
+    test_dir(100, "dir_100").await.unwrap();
+}
+
+
+#[ignore = "wip"]
+#[tokio::test]
+async fn test_1000() {
+    test_dir(1000, "dir_1000").await.unwrap();
+}
+
+
+#[ignore = "wip"]
+#[tokio::test]
+async fn test_10000() {
+    test_dir(10000, "dir_10000").await.unwrap();
+}
 
 fn get_config(dirname: &str, size: usize) -> FsConfig {
     let mut config = FsConfig::default();
@@ -18,26 +49,28 @@ fn get_config(dirname: &str, size: usize) -> FsConfig {
 }
 
 fn get_file_name(i: usize) -> String {
-    format!("file_{i}.txt")
+    format!("this_is_a_really_long_file_name_number_{i}_that_keeps_going_and_going_and_going_0123456789.txt")
 }
 
-#[ignore = "wip"]
-#[tokio::test]
-async fn test_10() {
-    const SIZE: usize = 10;
-    const DIR: &str = "big_dir";
-    let config = get_config(DIR, SIZE);
+async fn test_dir(size: usize, dir: &str) -> anyhow::Result<()> {
+    let config = get_config(dir, size);
     let mut client = TestContext::setup_with_config(config).await;
 
     let root_dir = client.root_dir().clone();
-    let dir = lookup(&mut client, root_dir.clone(), DIR).await.unwrap();
+    let dir = lookup(&mut client, root_dir.clone(), dir).await?;
 
-    for count in [1024*1024, 128*1024, 16*1024, 4*1024, 1024, 100] {
-        list_dir(&mut client, dir.clone(), SIZE, count).await.unwrap();
+    // going lower than 256 bytes will cause NFS3ERR_TOOSMALL
+    for count in [256 * 1024, 128 * 1024, 16 * 1024, 4 * 1024, 1024, 256] {
+        readdir(&mut client, dir.clone(), count, size).await?;
+        readdir_plus(&mut client, dir.clone(), count, count, size).await?;
+        readdir_plus(&mut client, dir.clone(), 1024*1024, count, size).await?;
+        readdir_plus(&mut client, dir.clone(), count, 1024*1024, size).await?;
     }
+
+    Ok(())
 }
 
-async fn lookup<IO: nfs3_client::io::AsyncRead + nfs3_client::io::AsyncWrite>(
+async fn lookup<IO: AsyncRead + AsyncWrite>(
     client: &mut TestContext<IO>,
     parent: nfs_fh3,
     name: &str,
@@ -54,19 +87,18 @@ async fn lookup<IO: nfs3_client::io::AsyncRead + nfs3_client::io::AsyncWrite>(
     Ok(lookup.object)
 }
 
-/*
-count
-    The maximum size of the READDIR3resok structure, in
-    bytes.  The size must include all XDR overhead. The
-    server is free to return less than count bytes of
-    data.
-*/
-async fn list_dir<IO: nfs3_client::io::AsyncRead + nfs3_client::io::AsyncWrite>(
+// count
+// The maximum size of the READDIR3resok structure, in
+// bytes.  The size must include all XDR overhead. The
+// server is free to return less than count bytes of
+// data.
+async fn readdir<IO: AsyncRead + AsyncWrite>(
     client: &mut TestContext<IO>,
     dir: nfs_fh3,
-    total_files_count: usize,
     count: u32,
+    total_files_count: usize,
 ) -> anyhow::Result<()> {
+    info!("readdir count: {count}");
 
     let mut cookie = 0;
     let mut cookieverf = cookieverf3::default();
@@ -81,15 +113,18 @@ async fn list_dir<IO: nfs3_client::io::AsyncRead + nfs3_client::io::AsyncWrite>(
         };
 
         let resok = client.readdir(args).await?.unwrap();
-        assert!(resok.packed_size() <= count as usize, "packed size is larger than count");
+        assert!(
+            resok.packed_size() <= count as usize,
+            "packed size is larger than count"
+        );
 
-        let dirlist3{entries, eof} = resok.reply;
+        let dirlist3 { entries, eof } = resok.reply;
         let entries = entries.into_inner();
         assert!(eof || entries.len() > 0, "eof is false but no entries");
 
         cookieverf = resok.cookieverf;
         cookie = entries.last().map_or(0, |e| e.cookie);
-        
+
         for entry in entries {
             let filename = String::from_utf8(entry.name.0.to_vec())?;
             assert!(all_entries.insert(filename), "duplicate entry");
@@ -102,11 +137,65 @@ async fn list_dir<IO: nfs3_client::io::AsyncRead + nfs3_client::io::AsyncWrite>(
 
     // Check if we have all entries
     assert_eq!(all_entries.len(), total_files_count);
-    println!("{all_entries:?}");
     for i in 0..total_files_count {
         let file_name = get_file_name(i);
         assert!(all_entries.contains(&file_name), "missing entry {i}");
     }
-    
+
+    Ok(())
+}
+
+async fn readdir_plus<IO: AsyncRead + AsyncWrite>(
+    client: &mut TestContext<IO>,
+    dir: nfs_fh3,
+    dircount: u32,
+    maxcount: u32,
+    total_files_count: usize,
+) -> anyhow::Result<()> {
+    info!("readdir_plus dircount: {dircount} maxcount: {maxcount}");
+
+    let mut cookie = 0;
+    let mut cookieverf = cookieverf3::default();
+    let mut all_entries = std::collections::HashSet::new();
+
+    loop {
+        let args = READDIRPLUS3args {
+            dir: dir.clone(),
+            cookie,
+            cookieverf: cookieverf.clone(),
+            dircount,
+            maxcount,
+        };
+
+        let resok = client.readdirplus(args).await?.unwrap();
+        assert!(
+            resok.packed_size() <= maxcount as usize,
+            "packed size is larger than count"
+        );
+
+        let dirlistplus3 { entries, eof } = resok.reply;
+        let entries = entries.into_inner();
+        assert!(eof || entries.len() > 0, "eof is false but no entries");
+
+        cookieverf = resok.cookieverf;
+        cookie = entries.last().map_or(0, |e| e.cookie);
+
+        for entry in entries {
+            let filename = String::from_utf8(entry.name.0.to_vec())?;
+            assert!(all_entries.insert(filename), "duplicate entry");
+        }
+
+        if eof {
+            break;
+        }
+    }
+
+    // Check if we have all entries
+    assert_eq!(all_entries.len(), total_files_count);
+    for i in 0..total_files_count {
+        let file_name = get_file_name(i);
+        assert!(all_entries.contains(&file_name), "missing entry {i}");
+    }
+
     Ok(())
 }
