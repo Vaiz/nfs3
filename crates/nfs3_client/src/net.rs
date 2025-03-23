212ae0956faaa3542a699f9b5d7e5d3e0c60f967
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-03-23 11:23:04 +0100

    fix typo

--- crates/nfs3_client/src/net.rs
+++ crates/nfs3_client/src/net.rs
@@ -10,7 +10,7 @@ pub trait Connector {
     /// Connect to a host and port.
     async fn connect(&self, host: &str, port: u16) -> std::io::Result<Self::Connection>;
 
-    /// Many NFS clients, especially on Linux, require that the source port used for NFS
+    /// Many NFS servers, especially on Linux, require that the source port used for NFS
     /// communication be in the privileged range (0-1023) by default.
     /// When the `local_port` is already in use, the function should return `AddrInUse` error.
     async fn connect_with_port(
