6d64d780339d57ece1053ade097868524b711309
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-04-06 15:56:22 +0200

    wip

--- crates/nfs3_server/src/portmap_handlers.rs
+++ crates/nfs3_server/src/portmap_handlers.rs
@@ -1,16 +1,15 @@
 use std::io::{Read, Write};
 
-use nfs3_types::portmap;
-use nfs3_types::portmap::PMAP_PROG;
+use nfs3_types::portmap::{self, PMAP_PROG, mapping};
 use nfs3_types::rpc::{accept_stat_data, call_body};
-use nfs3_types::xdr_codec::{Pack, Unpack};
+use nfs3_types::xdr_codec::{Pack, Unpack, Void};
 use tracing::{debug, error};
 
 use crate::context::RPCContext;
 use crate::rpc::{make_success_reply, proc_unavail_reply_message, prog_mismatch_reply_message};
 use crate::rpcwire::messages::{IncomingRpcMessage, OutgoingRpcMessage};
 
-pub fn handle_portmap_v2(    
+pub fn handle_portmap_v2(
     context: &RPCContext,
     message: IncomingRpcMessage,
 ) -> Result<Option<OutgoingRpcMessage>, anyhow::Error> {
@@ -23,7 +22,7 @@ pub fn handle_portmap_v2(
         );
         return Ok(Some(OutgoingRpcMessage::accept_error(
             message.xid(),
-            accept_stat_data::PROG_MISMATCH{  
+            accept_stat_data::PROG_MISMATCH {
                 low: portmap::VERSION,
                 high: portmap::VERSION,
             },
@@ -32,13 +31,48 @@ pub fn handle_portmap_v2(
 
     let prog = PMAP_PROG::try_from(call.proc);
     match prog {
-        // Ok(PMAP_PROG::PMAPPROC_NULL) => pmapproc_null(xid, input, output)?,
-        // Ok(PMAP_PROG::PMAPPROC_GETPORT) => pmapproc_getport(xid, input, output, context)?,
-        _ => {
-            Ok(Some(OutgoingRpcMessage::accept_error(
-                message.xid(),
-                accept_stat_data::PROC_UNAVAIL,
-            )))
+        Ok(PMAP_PROG::PMAPPROC_NULL) => pmapproc_null_v2(message),
+        Ok(PMAP_PROG::PMAPPROC_GETPORT) => pmapproc_getport_v2(context, message),
+        _ => Ok(Some(OutgoingRpcMessage::accept_error(
+            message.xid(),
+            accept_stat_data::PROC_UNAVAIL,
+        ))),
+    }
+}
+
+pub fn pmapproc_null_v2(
+    message: IncomingRpcMessage,
+) -> Result<Option<OutgoingRpcMessage>, anyhow::Error> {
+    debug!("pmapproc_null({})", message.xid());
+
+    match message.unpack_message::<Void>() {
+        Ok(_) => Ok(Some(message.into_success_reply(Box::new(Void)))),
+        Err(err) => {
+            error!("Failed to unpack pmapproc_null: {err}");
+            Ok(Some(
+                message.into_error_reply(accept_stat_data::GARBAGE_ARGS),
+            ))
+        }
+    }
+}
+
+// We fake a portmapper here. And always direct back to the same host port
+pub fn pmapproc_getport_v2(
+    context: &RPCContext,
+    message: IncomingRpcMessage,
+) -> Result<Option<OutgoingRpcMessage>, anyhow::Error> {
+    match message.unpack_message::<mapping>() {
+        Ok(mapping) => {
+            debug!("pmapproc_getport({}, {mapping:?})", message.xid());
+            let port = u32::from(context.local_port);
+            debug!("\t{} --> {}", message.xid(), port);
+            Ok(Some(message.into_success_reply(Box::new(port))))
+        }
+        Err(err) => {
+            error!("Failed to unpack pmapproc_null: {err}");
+            Ok(Some(
+                message.into_error_reply(accept_stat_data::GARBAGE_ARGS),
+            ))
         }
     }
 }
@@ -83,18 +117,6 @@ pub fn pmapproc_null(
     msg.pack(output)?;
     Ok(())
 }
-
-// pub fn null_v2(
-//     xid: u32,
-//     _input: Void,
-// ) -> Result<Void, anyhow::Error> {
-//     debug!("null_v2({:?}) ", xid);
-//     // build an RPC reply
-//     let msg = make_success_reply(xid);
-//     debug!("\t{:?} --> {:?}", xid, msg);
-//     msg.pack(output)?;
-//     Ok(())
-// }
 
 // We fake a portmapper here. And always direct back to the same host port
 pub fn pmapproc_getport(
