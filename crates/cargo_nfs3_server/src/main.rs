4caadca45f7ee833a8e6e2c969c8540c3d72b3fa
Vaiz <4908982+Vaiz@users.noreply.github.com>
2025-04-27 16:06:23 +0200

    move logging to another file

--- crates/cargo_nfs3_server/src/main.rs
+++ crates/cargo_nfs3_server/src/main.rs
@@ -1,13 +1,13 @@
 use std::path::Path;
-use std::vec;
 
 use clap::Parser;
 use nfs3_server::memfs::MemFs;
 use nfs3_server::tcp::NFSTcp;
 use nfs3_server::vfs::NfsFileSystem;
 use nfs3_server::vfs::adapter::ReadOnlyAdapter;
-use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
+use tracing_appender::non_blocking::WorkerGuard;
 
+mod logging;
 mod memfs;
 mod mirror;
 
@@ -55,7 +55,7 @@ struct Args {
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
-    let guards = init_logging(&args);
+    let guards = logging::init_logging(&args.log_level, args.log_file.as_deref(), !args.quiet);
 
     let bind_addr = format!("{}:{}", args.bind_ip, args.bind_port);
     let export_path = args.export_name.unwrap_or("/".to_owned());
@@ -122,96 +122,4 @@ async fn start_server(
         }
         _ = rx => { }
     }
-}
-
-fn init_logging(args: &Args) -> Vec<WorkerGuard> {
-    use tracing_subscriber::fmt::layer;
-    use tracing::subscriber::set_global_default;
-    use tracing_subscriber::layer::SubscriberExt;
-
-    let log_level = match args.log_level.to_lowercase().as_str() {
-        "error" => tracing::Level::ERROR,
-        "warn" => tracing::Level::WARN,
-        "info" => tracing::Level::INFO,
-        "debug" => tracing::Level::DEBUG,
-        "trace" => tracing::Level::TRACE,
-        _ => panic!("invalid log level: {}", args.log_level),
-    };
-
-    let level_filter = tracing_subscriber::filter::LevelFilter::from_level(log_level);
-    let subscriber = tracing_subscriber::Registry::default()
-        .with(level_filter);
-
-    match (args.quiet, args.log_file.as_deref()) {
-        (true, None) => {
-            // No logging
-            vec![]
-        }
-        (false, None) => {
-            // Console logging
-            let stdout_guard = init_stdout_logger();
-            let subscriber = subscriber
-                .with(layer().with_writer(stdout_logger));            
-            set_global_default(subscriber).expect("failed to set global subscriber");
-            vec![stdout_guard]
-        }
-        (true, Some(log_file)) => {
-            // File logging only
-            let file_guard = init_file_logger(log_file);
-            let subscriber = subscriber
-                .with(layer().with_writer(file_logger));
-            set_global_default(subscriber).expect("failed to set global subscriber");
-            vec![file_guard]
-        }
-        (false, Some(log_file)) => {
-            // both console and file logging
-            let stdout_guard = init_stdout_logger();
-            let file_guard = init_file_logger(log_file);
-            let subscriber = subscriber
-                .with(layer().with_writer(stdout_logger))
-                .with(layer().with_writer(file_logger));
-            set_global_default(subscriber).expect("failed to set global subscriber");
-            vec![stdout_guard, file_guard]
-        }
-    }
-}
-
-static STDOUT_LOGGER: std::sync::OnceLock<NonBlocking> = std::sync::OnceLock::new();
-static FILE_LOGGER: std::sync::OnceLock<NonBlocking> = std::sync::OnceLock::new();
-
-fn stdout_logger() -> impl std::io::Write {
-    STDOUT_LOGGER
-        .get()
-        .expect("stdout logger not initialzied")
-        .clone()
-}
-
-fn file_logger() -> impl std::io::Write {
-    FILE_LOGGER
-        .get()
-        .expect("file logger not initialized")
-        .clone()
-}
-
-fn init_stdout_logger() -> WorkerGuard {
-    let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
-    STDOUT_LOGGER
-        .set(non_blocking)
-        .expect("stdout logger already initialized");
-
-    guard
-}
-
-fn init_file_logger(log_file: &str) -> WorkerGuard {
-    let path = std::path::Path::new(log_file);
-    let file_appender = tracing_appender::rolling::never(
-        path.parent().unwrap_or_else(|| std::path::Path::new(".")),
-        path.file_name().expect("log file name is empty"),
-    );
-    let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
-    FILE_LOGGER
-        .set(non_blocking)
-        .expect("file logger already initialized");
-
-    guard
 }
